824289138a1319c3f2f357a5079959496592a9fb
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-06 09:24:25 +0100

    Updated signup page to be responsive

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import '../global.css'
 import { useNavigate } from 'react-router-dom';
 import {jwtDecode} from 'jwt-decode'
-import messageIcon from '../assets/SignUp/messageIcon.jpg'
+import storySetPic from '../assets/SignUp/Prototyping process-pana.png'
 import styles from './signup.module.css'
 import '../SignUpGlobalOverride.css'
 
@@ -31,40 +31,66 @@ function SignUp() {
                 localStorage.setItem('token', token);
 
                 const decodedToken = jwtDecode(token);
-                navigate('/');
+                // Navigate to the page where user can input bio, pic and ect.
+                navigate(`/user/${userId}/account`);
             }
         } catch (err) {
             setError('Log in failed. Please try again.')
         }
     }
-
-  return (
-    <>
+    
+    return (
+    <>  
         <div className={styles["signup-form-container"]}>
-            <form onSubmit={handleFormSubmit}>
-                <h1 className={styles.title}>Sign Up</h1>
-                <div className={styles['input-div']}>
-                    {error && <h3>{error}</h3>}
-                    <input 
-                        type='text'
-                        name='username'
-                        placeholder='Username'
-                        value={username}
-                        onChange={e => setUsername(e.target.value)}
-                    />
-                    <input
-                        type='password'
-                        name='password'
-                        placeholder='*********'
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                    />
-                </div>
-                <div className={styles['btn-container']}>
-                    <button type='submit' className={styles['signup-btn']}>Create Account</button>
-                </div>
-            </form>       
+            <div className={styles['signup-form-wrapper']}>
+                <form onSubmit={handleFormSubmit}>
+                    <div className={styles['title-div']}>
+                        <h1 className={styles.title}>Sign Up</h1>
+                    </div>
+                    <div className={styles['input-div']}>
+                        {error && <h3>{error}</h3>}
+                        <div className={styles['username']}>
+                            <label className={styles['username-label']}
+                                htmlFor='username'
+                            >
+                                Username
+                            </label>
+                            <input 
+                                type='text'
+                                name='username'
+                                placeholder='Username'
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div className={styles['password']}>
+                            <label className={styles['password-label']}
+                                htmlFor='password'
+                            >
+                                Password
+                            </label>
+                            <input
+                                type='password'
+                                name='password'
+                                placeholder='*********'
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className={styles['btn-container']}>
+                        <button type='submit' className={styles['signup-btn']}>Create Account</button>
+                    </div>
+                </form>
+                {/*
+                    <div className={styles['storyset-div']}>
+                        <img src={storySetPic} media="(min-width: 1024px)" alt="Sign Up Illustration" className={styles['storyset-pic']}/>
+                        <a href="https://storyset.com/app" className={styles['storyset-link']} media="(min-width: 1024px)">App illustrations by Storyset</a>
+                    </div>
+                */}     
+            </div>  
         </div>
+        
     </>
   )
 }

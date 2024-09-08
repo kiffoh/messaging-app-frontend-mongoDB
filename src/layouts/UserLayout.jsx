0b22389b028ff4ba599ab5c02bf9c44e0f32368c
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-08 15:51:43 +0100

    Update div styles

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 function UserLayout() {
   return (
-    <div>
+    <div style={{width:'100%', height:'100%'}}>
       <Outlet /> {/* This is where the nested routes will be rendered */}
     </div>
   );

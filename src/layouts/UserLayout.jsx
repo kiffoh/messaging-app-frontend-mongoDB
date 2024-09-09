dda069153a3d7a1e0a74a962c6bc2349a12798c7
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-09 10:43:13 +0100

    Update height and background colour

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 function UserLayout() {
   return (
-    <div style={{width:'100%', height:'100%'}}>
+    <div style={{width:'100%', height:'100vh', backgroundColor: 'var(--default-colour-light)'}}>
       <Outlet /> {/* This is where the nested routes will be rendered */}
     </div>
   );

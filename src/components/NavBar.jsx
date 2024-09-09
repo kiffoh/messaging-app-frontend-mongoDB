0b5ca113b55cc43406a1645be96ad24eed665dbf
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-09 10:46:26 +0100

    Profile picture shown in navbar
    User can click on picture to display the profile and signout options

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,23 +6,36 @@ import { useEffect, useState } from 'react';
 function NavBar() {
     const {user} = useAuth();
     const [userId, setUserId] = useState(null);
+    const [userPhoto, setUserPhoto] = useState('');
+    const [profileLinks, setProfileLinks] = useState(false)
 
     useEffect(() => {
         if (user) {
+            console.log(user)
             setUserId(user.id);
+            setUserPhoto(user.userPhoto);
         }
     }, [user])
 
     return (
         <div className={styles['navbar-body']}>
             <div className={styles['message-icon-container']}>
-                <img src={messageIcon} alt='message logo' className={styles['message-logo']}/>
+                <img src={messageIcon} alt='message logo' className={styles['message-logo']} draggable='false' />
             </div>
             <div className={styles.links}>
                 <a href='/'>Messages</a>
-                <div className={styles['profile-links']}>
-                    <a href={`/users/${userId}/profile`}>Profile</a>
-                    <a href='/users/signout'>Sign Out</a>
+                <div className={styles['profile-container']}>
+                    <div className={styles[(profileLinks? 'image-container-highlighted' : 'image-container')]}>
+                        {userPhoto && <img src={userPhoto} alt='User Photo' className={styles[(profileLinks? 'profile-picture-highlighted': 'profile-picture')]} onClick={() => setProfileLinks(!profileLinks)} draggable='false'/>}
+                    </div>
+                    {profileLinks && 
+                        <>
+                            <div className={styles['profile-links']}>
+                                <a href={`/users/${userId}/profile`}>Profile</a>
+                                <a href='/users/signout'>Sign Out</a>
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </div>

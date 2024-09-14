2d0628210faecb654dd8c789ce3b006198ceb805
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-14 15:15:39 +0100

    Update logic for creating new DirectMessage chat
    Initially searches userMessages to find a chat with the user. If none are found then one is created and added to the backend

--- src/components/DirectMessage.jsx
+++ src/components/DirectMessage.jsx
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from './chatContainer.module.css'
 import axios from "axios";
 const backendURL = import.meta.env.VITE_SERVER_URL
 
-function DirectMessage({setNewChat, filteredContacts, search, setSearch, setDisplayedChat, userChats}) {
+function DirectMessage({setNewChat, filteredContacts, search, setSearch, setDisplayedChat, userChats, user, setDisplayedChatId}) {
     const [error, setError] = useState(null);
+    const directMessageChats = userChats.filter(chat => chat.directMsg === true);
 
-    async function handleFormSubmit(chatId) {
-        
-
-        try {
-            // Need to filter this so that the direct message can be found e.g. search through members
-            const response = await axios(`${backendURL}/messages/${chatId}`);
-            if (response.ok) {
-                const data = response.json()
-                setDisplayedChat(data);
+    const findUserChat = (recipient) => {
+        for (const chat of directMessageChats) {
+            if (chat.members.some(member => member.id === recipient.id)) {
+                return chat;
             }
-
-        } catch (err) {
-            setError('An error occurred when trying to create the chat.')
         }
+        return null;
+    }
 
+
+    async function handleFormSubmit(recipient) {
+        const existingChat  = findUserChat(recipient)
+
+        if (existingChat) {
+            setDisplayedChat(existingChat);
+            setDisplayedChatId(existingChat.id);
+        } else {
+            try {
+                console.log('Triggering try loop')
+                // Need to filter this so that the direct message can be found e.g. search through members
+                const response = await axios.post(`${backendURL}/groups/create`, {
+                    members: [user, recipient]
+                });
+
+                if (response.status === 201) { // Check for successful creation
+                    const data = await response.json(); // Parse JSON data
+                    setDisplayedChat(data.newGroup); // Assuming response contains the new group in 'newGroup'
+                } else {
+                    setError('Failed to create the chat.');
+                }
+            } catch (err) {
+                setError('An error occurred when trying to create the chat.')
+            }
+        }
         setNewChat(false);
     }
 
@@ -39,7 +59,7 @@ function DirectMessage({setNewChat, filteredContacts, search, setSearch, setDisp
                 <p className={styles['no-contacts-found']}>No contacts found</p>
             ) : (
             filteredContacts.map(contact => (
-                <div key={contact.id} onClick={() => handleFormSubmit(contact.id)}>
+                <div key={contact.id} onClick={() => handleFormSubmit(contact)}>
                     <img src={contact.photo} alt='contact photo' className={styles['contact-photo']} />
                     <p className={styles['contact-name']}>{contact.username}</p>                  
                 </div>

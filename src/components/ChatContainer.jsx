91e9deb1fb2cd77a8127624deca9266955debaf5
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-15 15:52:55 +0100

    Added GroupMessage component
    Added a selected attribute to the contacts for GroupMessage logic

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -3,22 +3,26 @@ import styles from './chatContainer.module.css'
 import axios from 'axios';
 import DirectMessage from './DirectMessage';
 import DisplayedChat from './DisplayedChat';
+import GroupMessage from './GroupMessage';
 
 function ChatContainer({ user, displayedChat, setDisplayedChat, newChat, setNewChat, userChats, setDisplayedChatId, setUserChats }) {
-    const [error, setError] = useState(false)
     const [search, setSearch] = useState('')
     const [contacts, setContacts] = useState([]) // Need id's names and photos
+    const [newGroup, setNewGroup] = useState(false);
 
     const [filteredContacts, setFilteredContacts] = useState([]) // Need id's names and photos
 
-    const [members, setMembers] = useState([])
-
     useEffect(() => {
         if (user.contacts) {
-            setContacts(user.contacts);
-            setFilteredContacts(user.contacts);
+            const contactsWithSelection = user.contacts.map(contact => ({
+                ...contact,
+                selected: false // Initialize as not selected
+            }));
+            setContacts(contactsWithSelection);
+            setFilteredContacts(contactsWithSelection);
         }
     }, [user, newChat])
+
 
     useEffect(() =>{
         function filterContacts() {
@@ -35,21 +39,48 @@ function ChatContainer({ user, displayedChat, setDisplayedChat, newChat, setNewC
         filterContacts()
     }, [search, contacts])
 
+
+    // Update the selected state for a contact
+    function handleContactSelection(contactId) {
+        const updatedContacts = contacts.map(contact => 
+            contact.id === contactId ? { ...contact, selected: !contact.selected } : contact
+        );
+        setContacts(updatedContacts);
+        setFilteredContacts(updatedContacts);
+    }
+
     return (
         <>
             {newChat ? (
-                <DirectMessage 
-                    setNewChat={setNewChat}
-                    search={search}
-                    setSearch={setSearch}
-                    filteredContacts={filteredContacts}
-                    setDisplayedChat={setDisplayedChat}
-                    userChats={userChats}
-                    user={user}
-                    setDisplayedChatId={setDisplayedChatId}
-                    setUserChats={setUserChats}
-                />
-
+                newGroup ? (
+                    <GroupMessage 
+                        setNewChat={setNewChat}
+                        search={search}
+                        setSearch={setSearch}
+                        filteredContacts={filteredContacts}
+                        setDisplayedChat={setDisplayedChat}
+                        userChats={userChats}
+                        user={user}
+                        setDisplayedChatId={setDisplayedChatId}
+                        setUserChats={setUserChats}
+                        setNewGroup={setNewGroup}
+                        handleContactSelection={handleContactSelection}
+                        contacts={contacts}
+                    />
+                ) : (
+                    <DirectMessage 
+                        setNewChat={setNewChat}
+                        search={search}
+                        setSearch={setSearch}
+                        filteredContacts={filteredContacts}
+                        setDisplayedChat={setDisplayedChat}
+                        userChats={userChats}
+                        user={user}
+                        setDisplayedChatId={setDisplayedChatId}
+                        setUserChats={setUserChats}
+                        setNewGroup={setNewGroup}
+                    />
+                )
             ) : (
                 displayedChat && <DisplayedChat displayedChat={displayedChat} user={user} />
             )}

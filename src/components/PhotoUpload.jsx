cd55bd7f74dcf26db4bb7a5f6b018043f265b96d
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-09-22 14:19:11 +0100

    Create PhotoUpload component
    Integrates uploading files with a photo icon

---
+++ src/components/PhotoUpload.jsx
@@ -0,0 +1,42 @@
+import { useRef } from 'react';
+import { FaCamera } from 'react-icons/fa'; // Importing a camera icon (FontAwesome)
+import styles from './userProfile.module.css'
+
+const PhotoUpload = ({file, setFile, className}) => {
+    const fileInputRef = useRef(null);
+
+    const handleIconClick = () => {
+        fileInputRef.current.click(); // Trigger the file input click
+    };
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]); // Handle the selected file
+    };
+
+    return (
+        <div className={`${styles['photo-upload-body']} ${className}`}>
+            {/* Hidden file input */}
+            <input
+                type="file"
+                ref={fileInputRef} // Assign reference
+                style={{ display: 'none' }} // Hide the input
+                onChange={handleFileChange}
+                name="photo"
+            />
+
+            {/* Camera icon that triggers the file input */}
+            <button type="button" onClick={handleIconClick} style={{ background: 'none', border: 'none' }} className={`${styles['photo-icon']} ${className}`}>
+                <FaCamera size={24} />
+            </button>
+
+            {/* Display the selected file (optional) */}
+            {file ? 
+                file.name && <p style={{margin:0}}>Selected file: {file.name}</p>
+            :
+                <p style={{margin:0}}>No file selected</p>}
+        </div>
+    );
+};
+
+
+export default PhotoUpload;

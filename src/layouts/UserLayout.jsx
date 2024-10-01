be25958af8503abc90064ed306c86c75e35ea91c
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-10-01 16:52:13 +0100

    Styling for respective scrollbars

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 function UserLayout() {
   return (
-    <div style={{width:'100%', height:'100vh', backgroundColor: 'var(--default-colour-light)'}}>
+    <div style={{width:'100%', height:'100vh', backgroundColor: 'var(--default-colour-light)', display:'grid', gridTemplateRows:'auto 1fr'}}>
       <Outlet /> {/* This is where the nested routes will be rendered */}
     </div>
   );

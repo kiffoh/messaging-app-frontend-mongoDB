339e1a47c10dcccef2a5b4f290fcae2c8129cb29
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-10-15 16:19:51 +0100

    Re-direct if user not logged in

--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,6 +1,21 @@
 import { Outlet } from 'react-router-dom';
+import useAuth from '../Authentification/useAuth';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function UserLayout() {
+  const {user} = useAuth();
+  const [userLoading, setUserLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      setUserLoading(false);
+  } else if (!userLoading && !user) {
+      navigate('/users/login');
+  }  
+  }, [user, userLoading, navigate])
+
   return (
     <div style={{width:'100%', height:'100vh', backgroundColor: 'var(--default-colour-light)'}}>
       <Outlet /> {/* This is where the nested routes will be rendered */}

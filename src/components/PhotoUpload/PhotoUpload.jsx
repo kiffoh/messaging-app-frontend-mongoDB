5c4bcc234ef46c41ea754299e6818410fe419505
Kiff <125292279+kiffoh@users.noreply.github.com>
2024-10-18 16:23:58 +0100

    Corrected styles import

--- src/components/PhotoUpload/PhotoUpload.jsx
+++ src/components/PhotoUpload/PhotoUpload.jsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { FaCamera } from 'react-icons/fa'; // Importing a camera icon (FontAwesome)
-import styles from '../UserProfile/userProfile.module.css'
+import styles from '../UserProfile/userprofile.module.css'
 
 const PhotoUpload = ({file, setFile, className}) => {
     const fileInputRef = useRef(null);
